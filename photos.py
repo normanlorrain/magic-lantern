ec67616687c23acb4e03fd15d17b35aa8809401d
Norman Lorrain <normanlorrain@gmail.com>
2024-01-09 14:05:00 -0700

    First steps.

---
+++ photos.py
@@ -0,0 +1,21 @@
+import os
+import logging as log
+
+
+photoList = []
+
+
+def findPhotos(src: str):
+    for root, dirs, files in os.walk(src):
+        log.info(f"In {root}")
+        for f in files:
+            if not f.lower().endswith((".png", ".jpg", ".jpeg")):
+                log.warning(f"{f}  Unknown file type")
+                continue
+            photoList.append(f)
+
+            # try:
+            #     y, m, d = getDateFromFile(root, f)
+            # except ValueError as e:
+            #     log.warning(f"{f}, {e}, (File not processed)")
+            #     continue
