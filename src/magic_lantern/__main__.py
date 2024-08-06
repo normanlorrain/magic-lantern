08ce3d590b7967f9c975e80a7642caf0afa1d1a3
Norman Lorrain <normanlorrain@gmail.com>
2024-08-06 10:31:01 -0600

    comments

--- src/magic_lantern/__main__.py
+++ src/magic_lantern/__main__.py
@@ -1,7 +1,9 @@
 import os
 from magic_lantern import cli
 
-if "magic_lantern_PROFILE" in os.environ:
+# To invoke the profiler, set this environment variable and run as a module
+# i.e. python -m magic_lantern ....
+if "MAGIC_LANTERN_PROFILE" in os.environ:
     import cProfile
 
     cProfile.run("cli()", sort="time")
