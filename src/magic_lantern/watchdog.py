cc8c70964af2c45c126925e4da7e614024496be0
Norman Lorrain <normanlorrain@gmail.com>
2024-08-21 16:53:48 -0600

    redundtant since we validate configuration now.

--- src/magic_lantern/watchdog.py
+++ src/magic_lantern/watchdog.py
@@ -15,11 +15,6 @@ def init():
     _observer = Observer()
     for dictAlbum in config.albums:
         path = Path(dictAlbum[config.FOLDER])
-        if not path.is_absolute():
-            path = config.configRoot / path
-        if not path.exists():
-            raise Exception(f"bad Config: invalid path: {path}")
-
         event_handler = EventHandler()
         _observer.schedule(event_handler, path, recursive=True)
         log.info(f"monitoring: {path}")
